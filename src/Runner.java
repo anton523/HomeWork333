3263b8b36294c49f59ecf5dfee5c25e4ddd62af4
anton523 <52636954+anton523@users.noreply.github.com>
2024-06-17 16:12:30 +0500

    Update Runner.java

--- src/Runner.java
+++ src/Runner.java
@@ -1,2 +1,49 @@
-package PACKAGE_NAME;public class Runner {
+import java.util.Comparator;
+
+public class Runner {
+    public static void main(String[] args) {
+        MyArrayList<String> list= new MyArrayList<>();
+        MyArrayList<String> list2= new MyArrayList<>();
+        list2.add("дыня");
+        list2.add("арбуз");
+        list2.add("тыква");
+        list2.add("апельсин");
+        list2.add(2,"грязь");
+        System.out.println(list2);
+
+        list.add("0");
+        list.add("1");
+        list.add("2");
+        list.add("3");
+        list.add("4");
+        System.out.println(list+"\n");
+
+        list.addAll(2,list2);
+
+        System.out.println(list);
+
+        list.remove(2);
+        list.remove();
+
+        System.out.println(list);
+        list.add("q");
+        list.add("q");
+        list.add("q");
+        System.out.println(list);
+
+        list.sort(Comparator.naturalOrder()); // Сортировка по лексике
+
+        System.out.println(list);
+
+
+        System.out.println(list.get(7));
+        MyArrayList<String> list3=new MyArrayList<>(0);
+        list3.add("qq");
+        list3.add("qq");
+        list3.add("qq");
+        System.out.println(list3);
+
+
+
+    }
 }
